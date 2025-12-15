6d1c92b54841066e1c5f28af219a62f4d55f4521
Apaarmeet <apaarmeet5000@gmail.com>
2025-12-16 01:24:17 +0530

    deploy commands

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 git pull 
-/home/ubuntu/.nvm/versions/node/v24.11.1/bin/npm install
-/home/ubuntu/.nvm/versions/node/v24.11.1/bin/npm run build
-/home/ubuntu/.nvm/versions/node/v24.11.1/bin/npm run start
+npm install
+npm run build
+npm run start
